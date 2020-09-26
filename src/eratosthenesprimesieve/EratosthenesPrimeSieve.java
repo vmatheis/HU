3ddb8df26bd5f03de58ab1996b2fa936b3fdf590
vmatheis <Matheis.Vanessa@sus.htl-grieskirchen.at>
2020-09-26 17:08:51 +0200

    3. Speicherung

--- src/eratosthenesprimesieve/EratosthenesPrimeSieve.java
+++ src/eratosthenesprimesieve/EratosthenesPrimeSieve.java
@@ -5,17 +5,50 @@
  */
 package eratosthenesprimesieve;
 
+import java.util.Scanner;
+
 /**
  *
  * @author vmatheis
  */
-public class EratosthenesPrimeSieve {
+public class EratosthenesPrimeSieve implements PrimeSieve {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    int border;
+    boolean[] prims;
+
+    public EratosthenesPrimeSieve(int p) {
+        border = p;
+        prims = new boolean[border + 1];
     }
-    
+
+    public static void main(String[] args) {
+        
+    }
+
+    //1. Beispiel
+    @Override
+    public boolean isPrime(int p) {
+        boolean isprim = false;
+        boolean[] prime = new boolean[p + 1];
+        for (int i = 0; i < p; i++) {
+            prime[i] = true;
+        }
+
+        for (int x = 2; x * x <= p; x++) {
+            if (prime[p] == true) {
+                for (int i = x * 2; i <= x; i += x) {
+                    prime[i] = false;
+                }
+            }
+            isprim = true;
+        }
+
+        prims = prime;
+        return isprim;
+    }
+
+    @Override
+    public void printPrimes() {
+        
+    }
 }
